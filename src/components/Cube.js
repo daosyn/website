5229096c05873b2285de24d98380de9d721e1781
daosyn <brandon@daosyn.info>
2020-01-14 16:03:45 -0600

    adjust dimensions, mess with overflow

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -2,31 +2,34 @@ import m from "mithril";
 import Face from "./Face";
 
 const Cube = {
-  counter: 0,
-  margin: "0px",
-  top: "0px",
-  faceletArr: [...Array(36).keys()],
-  faces: ["r-face", "l-face", "f-face", "b-face", "u-face", "d-face"],
-  links: ["/blog", "/about", "/gallery"],
-  getAttributes: function(i) {
-    const face = this.faces[i % 6];
-    return "facelet " + face;
-  },
+  // hardcoded atm
+  // oninit, check window height and width
   //   let height = window.innerHeight;
   //   let width = window.innerWidth;
   //   this.size =
   //     (height >= width ? Math.ceil(width / 9) : Math.ceil(height / 9)) + "px";
+  counter: 0,
+  margin: "0px",
+  top: "0px",
+  faceletArr: [...Array(64).keys()],
+  faces: ["r-face", "l-face", "f-face", "b-face", "u-face", "d-face"],
+  links: ["/blog", "/about", "/gallery"],
+  getAttributes: function(i) {
+    // calculate column here for now
+    const face = this.faces[i % 6];
+    return "facelet " + face;
+  },
   move: function() {
     // just increment margins
     // use transitions to move squares
-    if (parseInt(this.margin) > parseInt(window.innerWidth) - 280) {
+    if (parseInt(this.margin) > parseInt(window.innerWidth) - 200) {
       this.margin = "0px";
-      this.top = parseInt(this.top) + 140 + "px";
+      this.top = parseInt(this.top) + 100 + "px";
       if (parseInt(this.top) > parseInt(window.innerHeight) / 2) {
         this.top = "0px";
       }
     } else {
-      this.margin = parseInt(this.margin) + 140 + "px";
+      this.margin = parseInt(this.margin) + 100 + "px";
     }
   },
   view: function() {
@@ -36,10 +39,8 @@ const Cube = {
         return m("div", {
           class: this.getAttributes(value),
           style: {
-            width: "100px",
-            height: "100px",
-            marginLeft: this.margin,
-            marginTop: value < 6 ? this.top : null
+            marginLeft: value < 6 ? this.margin : value >= 36 ? "-100px" : null
+            // marginTop: value < 6 ? this.top : null
           },
           onclick: () => {
             console.log(this.links[Math.floor(Math.random() * 3)]);
