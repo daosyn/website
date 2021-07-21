ca964417acc728860ebaee02cc0ec938de70b659
daosyn <brandon@daosyn.info>
2021-07-21 02:52:46 -0500

    implement l/r shifting

---
+++ src/components/Cube.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div v-for="(row, y) in faceletMatrix" :key="y">
+      <span v-for="(facelet, x) in row" :key="x">{{ facelet }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cube",
+  props: {},
+  created() {
+    setInterval(() => {
+      const index = Math.floor(Math.random() * 6);
+      const direction = Math.floor(Math.random() * 2);
+      direction ? this.moveLeft(index) : this.moveRight(index);
+    }, 1000);
+  },
+  data() {
+    return {
+      faceletMatrix: [
+        [0, 1, 2, 3, 4, 5],
+        [0, 1, 2, 3, 4, 5],
+        [0, 1, 2, 3, 4, 5],
+        [0, 1, 2, 3, 4, 5],
+        [0, 1, 2, 3, 4, 5],
+        [0, 1, 2, 3, 4, 5],
+      ],
+    };
+  },
+  methods: {
+    moveUp() {},
+    moveDown() {},
+    moveLeft(row) {
+      this.faceletMatrix[row].push(this.faceletMatrix[row].shift());
+    },
+    moveRight(row) {
+      this.faceletMatrix[row].unshift(this.faceletMatrix[row].pop());
+    },
+  },
+};
+</script>
+
+<style>
+</style>
+
+{
+    index
+    color
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Cube />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Cube from "@/components/Cube.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    Cube,
+  },
+};
+</script>
