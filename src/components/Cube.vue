3c0e5f4e161a40d626700c76ddc6365b31919a87
daosyn <brandon@daosyn.info>
2021-07-21 03:36:21 -0500

    implement up/down, add colors

--- src/components/Cube.vue
+++ src/components/Cube.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="(row, y) in faceletMatrix" :key="y">
-      <span v-for="(facelet, x) in row" :key="x">{{ facelet }}</span>
+      <span v-for="(facelet, x) in row" :key="x" :class="facelet">o</span>
     </div>
   </div>
 </template>
@@ -14,24 +14,44 @@ export default {
     setInterval(() => {
       const index = Math.floor(Math.random() * 6);
       const direction = Math.floor(Math.random() * 2);
-      direction ? this.moveLeft(index) : this.moveRight(index);
+      const axis = Math.floor(Math.random() * 2);
+      if (axis) {
+        direction ? this.moveLeft(index) : this.moveRight(index);
+      } else {
+        direction ? this.moveUp(index) : this.moveDown(index);
+      }
     }, 1000);
   },
   data() {
     return {
       faceletMatrix: [
-        [0, 1, 2, 3, 4, 5],
-        [0, 1, 2, 3, 4, 5],
-        [0, 1, 2, 3, 4, 5],
-        [0, 1, 2, 3, 4, 5],
-        [0, 1, 2, 3, 4, 5],
-        [0, 1, 2, 3, 4, 5],
+        ["color-0", "color-1", "color-2", "color-3", "color-4", "color-5"],
+        ["color-0", "color-1", "color-2", "color-3", "color-4", "color-5"],
+        ["color-0", "color-1", "color-2", "color-3", "color-4", "color-5"],
+        ["color-0", "color-1", "color-2", "color-3", "color-4", "color-5"],
+        ["color-0", "color-1", "color-2", "color-3", "color-4", "color-5"],
+        ["color-0", "color-1", "color-2", "color-3", "color-4", "color-5"],
+        // [0, 1, 2, 3, 4, 5],
       ],
     };
   },
   methods: {
-    moveUp() {},
-    moveDown() {},
+    moveUp(column) {
+      let prev = this.faceletMatrix[0][column];
+      for (var i = this.faceletMatrix.length - 1; i >= 0; i--) {
+        let temp = this.faceletMatrix[i][column];
+        this.faceletMatrix[i][column] = prev;
+        prev = temp;
+      }
+    },
+    moveDown(column) {
+      let prev = this.faceletMatrix[this.faceletMatrix.length - 1][column];
+      for (const facelet of this.faceletMatrix) {
+        let temp = facelet[column];
+        facelet[column] = prev;
+        prev = temp;
+      }
+    },
     moveLeft(row) {
       this.faceletMatrix[row].push(this.faceletMatrix[row].shift());
     },
@@ -43,9 +63,22 @@ export default {
 </script>
 
 <style>
+ .color-0 {
+    background: red;
+ }
+ .color-1 {
+    background: blue;
+ }
+ .color-2 {
+    background: white;
+ }
+ .color-3 {
+    background: orange;
+ }
+ .color-4 {
+    background: green;
+ }
+ .color-5 {
+    background: yellow;
+ }
 </style>
-
-{
-    index
-    color
-}
