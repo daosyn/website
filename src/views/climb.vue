c4adac8886c55e5aad5bcf93a302cc9576d5c500
daosyn <brandon@daosyn.info>
2023-10-04 15:04:28 -0500

    disallow fullscreen

--- src/views/climb.vue
+++ src/views/climb.vue
@@ -7,7 +7,6 @@
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
     ></iframe>
   </div>
 </template>
--- src/views/youtube.vue
+++ src/views/youtube.vue
@@ -7,7 +7,6 @@
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
     />
   </div>
 </template>
