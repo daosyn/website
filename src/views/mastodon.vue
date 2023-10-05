9803472b41d13569e6e6797894e1a18583b5c3e4
daosyn <brandon@daosyn.info>
2023-10-04 22:24:21 -0500

    try mastofeed with https

--- src/views/mastodon.vue
+++ src/views/mastodon.vue
@@ -5,6 +5,6 @@
     sandbox="allow-top-navigation allow-scripts allow-popups allow-popups-to-escape-sandbox"
     width="75%"
     height="75%"
-    src="http://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fmastodon.social%2Fusers%2Fdaosyn&theme=dark&size=100&header=false&replies=false&boosts=false"
+    src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fmastodon.social%2Fusers%2Fdaosyn&theme=dark&size=100&header=false&replies=false&boosts=false"
   />
 </template>
