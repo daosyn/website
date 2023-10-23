a346f24e2c1dac00c7fef0b299c4d3d7626347d0
daosyn <brandon@daosyn.info>
2023-10-23 10:51:01 -0500

    add 'deploy' to git msg

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
     sed -i "" '/dist/d' ./.gitignore
     git add .
-    git commit -m "$(git log -1 --pretty=format:%B)"
+    git commit -m "deploy '$(git log -1 --pretty=format:%B)'"
     git push origin `git subtree split --prefix dist master`:gh-pages --force
     git reset HEAD~
     git checkout .gitignore
