a86b604dca6b4d072ff096e4898164e469660c70
daosyn <brandon@daosyn.info>
2023-10-23 10:37:44 -0500

    make mastodon more responsive

--- src/views/mastodon.vue
+++ src/views/mastodon.vue
@@ -1,10 +1,33 @@
 <template>
-  <iframe
-    title="asi"
-    allowfullscreen
-    sandbox="allow-top-navigation allow-scripts allow-popups allow-popups-to-escape-sandbox"
-    width="75%"
-    height="75%"
-    src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fmastodon.social%2Fusers%2Fdaosyn&theme=dark&size=100&header=false&replies=false&boosts=false"
-  />
+  <div class="mastodon-container">
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fmastodon.social%2Fusers%2Fdaosyn&theme=dark&size=100&header=false&replies=false&boosts=false"
+      title="mastodon feed"
+      frameborder="0"
+      sandbox="allow-top-navigation allow-scripts allow-popups allow-popups-to-escape-sandbox"
+    />
+  </div>
 </template>
+
+<style scoped>
+.mastodon-container {
+  position: relative;
+  overflow: hidden;
+}
+
+.mastodon-container::after {
+  display: block;
+  content: "";
+  padding-top: 56.25%;
+}
+
+.mastodon-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
