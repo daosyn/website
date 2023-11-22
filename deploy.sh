40a7534541950c1f883757b77dda4243fb0f1486
daosyn <brandon@daosyn.info>
2023-11-21 23:30:16 -0600

    switch master to main

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ then
     sed -i "" '/dist/d' ./.gitignore
     git add .
     git commit -m "deploy '$(git log -1 --pretty=format:%B)'"
-    git push origin `git subtree split --prefix dist master`:gh-pages --force
+    git push origin `git subtree split --prefix dist main`:gh-pages --force
     git reset HEAD~
     git checkout .gitignore
 else
