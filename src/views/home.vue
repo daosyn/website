d04300355ff22eca90d5df7f8690f28da7b6a5fd
daosyn <brandon@daosyn.info>
2024-03-26 17:15:42 -0500

    add href to lastm, adjust image scaling

--- src/views/home.vue
+++ src/views/home.vue
@@ -5,21 +5,6 @@
       {{ today }}
     </h2>
     <img src="/images/inmato.jpg" alt="inmato" />
-    <!-- <h3>
-      <router-link to="/lastfm">// currently listening to //</router-link>
-    </h3>
-    <h3>
-      <router-link to="/goodreads">// currently reading //</router-link>
-    </h3>
-    <h3>
-      <router-link to="/youtube">// currently singing //</router-link>
-    </h3>
-    <h3>
-      <router-link to="/mastodon">// currently thinking about //</router-link>
-    </h3>
-    <h3>
-      <router-link to="/climb">// recently climbed //</router-link>
-    </h3> -->
   </div>
 </template>
 
@@ -56,9 +41,15 @@ export default {
 h1 {
   color: crimson;
   text-decoration: none;
+  font-size: 3vh;
+}
+
+h2 {
+  font-size: 2vh;
 }
 
 img {
-  width: 50%;
+  width: 69%;
+  object-fit: scale-down;
 }
 </style>
--- src/views/lastfm.vue
+++ src/views/lastfm.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-    <img
-      alt="lastfm collage"
-      src="https://tapmusic.net/collage.php?user=daosyn&type=1month&size=3x3"
-      style="width: 100%"
-    />
+    <a href="https://www.last.fm/user/daosyn" target="_blank">
+      <img
+        alt="lastfm collage"
+        src="https://tapmusic.net/collage.php?user=daosyn&type=1month&size=3x3"
+        :style="{
+          height: isLandscape ? 'auto' : '100%',
+          width: isLandscape ? '100%' : 'auto',
+        }"
+      />
+    </a>
   </div>
 </template>
+
+<script>
+export default {
+  setup: () => {},
+  data: () => ({
+    isLandscape: window.innerWidth > window.innerHeight,
+  }),
+  mounted() {
+    window.addEventListener("resize", () => {
+      this.isLandscape = window.innerWidth > window.innerHeight;
+    });
+  },
+};
+</script>
