0b1629d295b3b6327c985c31049e4ad577335c92
daosyn <brandon@daosyn.info>
2024-04-09 11:16:37 -0500

    fix 404, mstdn full page

--- assets/climb-230aecc6.js
+++
@@ -1,1 +0,0 @@
-import{y as t}from"./youtube-1fb4147e.js";import{_ as e,r as s,o as n,d as r}from"./index-91aa3cda.js";import"./loader-e441695d.js";const c={components:{youtube:t}};function p(a,u,_,m,i,d){const o=s("youtube");return n(),r(o,{"playlist-id":"PLuZs8bwd7syXDVOG6oUa9pOtyISSt3uND"})}const b=e(c,[["render",p]]);export{b as default};
