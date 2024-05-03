74283b42adb1f9c849793d28db2091284c430180
daosyn <brandon@daosyn.info>
2024-05-02 19:28:12 -0500

    add 1 buffer, some photots

---
+++ assets/climb-64e3ac2b.js
@@ -0,0 +1,1 @@
+import{y as t}from"./youtube-e9b22530.js";import{_ as e,r as s,o as n,d as r}from"./index-a26828f4.js";import"./loader-a9c016ff.js";const c={components:{youtube:t}};function p(a,u,_,m,i,d){const o=s("youtube");return n(),r(o,{"playlist-id":"PLuZs8bwd7syXDVOG6oUa9pOtyISSt3uND"})}const b=e(c,[["render",p]]);export{b as default};
---
+++ assets/covers-7d98e56a.js
@@ -0,0 +1,1 @@
+import{y as e}from"./youtube-e9b22530.js";import{_ as t,r as s,o as c,d as r}from"./index-a26828f4.js";import"./loader-a9c016ff.js";const n={components:{youtube:e}};function p(a,u,_,m,i,d){const o=s("youtube");return c(),r(o,{"playlist-id":"PLuZs8bwd7syWgz3LE28M1cug9IPceObC3"})}const b=t(n,[["render",p]]);export{b as default};
