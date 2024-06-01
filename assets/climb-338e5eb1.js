06d6eeb682a71def3bd1cf01823e08f107eae373
daosyn <brandon@daosyn.info>
2024-06-01 00:36:06 -0500

    update resume, fix typo

--- assets/climb-338e5eb1.js
+++
@@ -1,1 +0,0 @@
-import{y as t}from"./youtube-a78cdac2.js";import{_ as e,r as s,o as n,d as r}from"./index-c45ed8db.js";import"./loader-097565cb.js";const c={components:{youtube:t}};function p(a,u,_,m,i,d){const o=s("youtube");return n(),r(o,{"playlist-id":"PLuZs8bwd7syXDVOG6oUa9pOtyISSt3uND"})}const b=e(c,[["render",p]]);export{b as default};
