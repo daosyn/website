c7ea06f7e707bf6b676e9ae6fa6b76bdd3a06785
daosyn <brandon@daosyn.info>
2026-08-11 10:10:15 -0500

    add www. to tapmusic src

--- src/views/lastfm.vue
+++ src/views/lastfm.vue
@@ -3,7 +3,7 @@
     <a href="https://www.last.fm/user/daosyn" target="_blank">
       <img
         alt="lastfm collage"
-        src="https://tapmusic.net/collage.php?user=daosyn&type=1month&size=3x3"
+        src="https://www.tapmusic.net/collage.php?user=daosyn&type=1month&size=3x3"
         :height="size"
         :width="size"
         @load="handleLoad"
